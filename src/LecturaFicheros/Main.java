ae0581405c60b677c000fff62432d8df991c4fff
sergiomenurt <smendezurtaza@danielcastelao.org>
2022-06-05 18:51:19 +0200

    Initial commit

---
+++ src/LecturaFicheros/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LecturaFicheros;
+
+import Tractor.Principal;
+import javax.swing.JOptionPane;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+      JOptionPane.showMessageDialog (null,"MINIJUEGO TRACTOR");
+
+
+      JOptionPane.showMessageDialog(null, " Bienvenido Jugador! Estás listo?");
+      
+        Principal.PrimerMinijuego();
+
+    }
+}
+
